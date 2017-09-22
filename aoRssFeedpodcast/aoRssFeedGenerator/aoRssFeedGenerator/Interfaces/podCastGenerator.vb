dedcfd0bfdbe049bec42437a86d10cda80ab620b
vsalmiery1 <M!pa55w0rd>
2017-09-22 17:41:55 -0400

    update

---
+++ aoRssFeedpodcast/aoRssFeedGenerator/aoRssFeedGenerator/Interfaces/podCastGenerator.vb
@@ -0,0 +1,81 @@
+
+Option Strict On
+Option Explicit On
+
+Imports Contensive.BaseClasses
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Xml
+Imports System.Xml.Schema
+Imports System.IO
+Imports System.Text
+
+Namespace Interfaces
+    '
+    Public Class podCastGenerator
+        Inherits AddonBaseClass
+        '
+        ' - if nuget references are not there, open nuget command line and click the 'reload' message at the top, or run "Update-Package -reinstall" - close/open
+        ' - Verify project root name space is empty
+        ' - Change the namespace (AddonCollectionVb) to the collection name
+        ' - Change this class name to the addon name
+        ' - Create a Contensive Addon record with the namespace apCollectionName.ad
+        '
+        '=====================================================================================
+        ''' <summary>
+        ''' AddonDescription
+        ''' </summary>
+        ''' <param name="CP"></param>
+        ''' <returns></returns>
+        Public Overrides Function Execute(ByVal CP As CPBaseClass) As Object
+            Dim result As String = ""
+            Try
+                '
+                ' -- initialize application. If authentication needed and not login page, pass true
+                Using ae As New Controllers.applicationController(CP, False)
+                    '
+                    ' -- create just one channel
+                    Dim channelargument As New RSSFeed.channelArgumentsClass()
+                    channelargument.categories.Add("Comedy", "somethingElseWeDontKnow")
+                    channelargument.title = "The Big O And Dukes"
+                    channelargument.link = "http://www.bigoanddukes.com/home"
+                    channelargument.language = "en-us"
+                    channelargument.description = "With a 10 year passionate terrestrial radio following, The Big O and Dukes Show has a mind-blowing 4 million downloads to date. This current incarnation is its purest form to date. From pop-culture to politics, each approaches subject matter from very different perspectives. So pull up a seat, plug in your headphones and step into the studio with Chad Dukes, Oscar Santana and Drab T-Shirt. By the end of each show, you\'ll have laughed yourself right out of your chair, but have gained a window into the lives of three new friends."
+                    channelargument.copyRight = " &#x2117; &amp; &#xA9; 2011 Big O And Dukes"
+                    channelargument.itunessubtitle = "A show about everything"
+                    channelargument.itunesauthor = "Big O And Dukes"
+                    channelargument.itunessummary = "With a 10 year passionate terrestrial radio following, The Big O and Dukes Show has a mind-blowing 4 million downloads to date. This current incarnation is its purest form to date. From pop-culture to politics, each approaches subject matter from very different perspectives. So pull up a seat, plug in your headphones and step into the studio with Chad Dukes, Oscar Santana and Drab T-Shirt. By the end of each show, you\'ll have laughed yourself right out of your chair, but have gained a window into the lives of three new friends."
+                    channelargument.ituneskeywords = "big o and dukes, big o and dukes show, oscar santana, chad dukes, mike o\'meara show, opie and anthony, howard stern, adam carolla show, kevin smith, tom leykis, gary and dino, don geronimo"
+                    channelargument.itunesexplicit = "Yes"
+                    channelargument.itunesnewfeedurl = "http://bigoanddukes.com/feed/podcast"
+                    'channelargument.dateLastChanged = ?
+                    channelargument.ownerName = "Big O And Dukes"
+                    channelargument.ownerEmail = "bigoanddukes@gmail.com"
+                    channelargument.feedimage = " href=""http://bigoanddukes.com/feed/podcast/boad-itunes.png"""
+                    Dim rssfeed As New RSSFeed()
+                    rssfeed.CreateChannel(channelargument)
+                    '
+                    ' -- add items
+                    Dim sqlCriteria As String = "((isBlack=0)or(isBlack is null))and(isNetwork=1)and(isPremium<>1)and(link<>'')and(link is not null)and(name<>'')and(name is not null)"
+                    Dim orderBy As String = "dateadded desc"
+                    Dim createdbyAuth As String = "Big O and Dukes"
+                    Dim podCastList As List(Of Models.BoadPodcastModel) = Models.BoadPodcastModel.createList(CP, "", orderBy)
+                    For Each podCast As Models.BoadPodcastModel In podCastList
+
+                        rssfeed.WriteRSSItem(podCast.name, podCast.link, "Big O And Dukes", podCast.RSSDescription, "", "Comedy", "", podCast.ccguid, "Yes", podCast.RSSDatePublish, channelargument.ituneskeywords, podCast.imageFilename,)
+
+                    Next
+                    result = CP.Utils.EncodeHTML(rssfeed.ToString)
+                    If ae.packageErrorList.Count > 0 Then
+                        result = "Hey user, this happened - " & Join(ae.packageErrorList.ToArray, "<br>")
+                    End If
+                End Using
+            Catch ex As Exception
+                CP.Site.ErrorReport(ex)
+            End Try
+            Return result
+        End Function
+    End Class
+End Namespace
+
---
+++ aoRssFeedpodcast/aoRssFeedGenerator/aoRssFeedGenerator/Models/BoadPodcastModel.vb
@@ -0,0 +1,126 @@
+﻿
+Option Explicit On
+Option Strict On
+
+Imports System
+Imports System.Collections.Generic
+Imports System.Text
+Imports Contensive.BaseClasses
+
+Namespace Models
+    Public Class BoadPodcastModel
+        Inherits baseModel
+        Implements ICloneable
+        '
+        '====================================================================================================
+        '-- const
+        Public Const contentName As String = "Boad Podcasts"                   '<------ set content name
+        Public Const contentTableName As String = "boadPodcasts"            '<------ set to tablename for the primary content (used for cache names)
+        Private Shadows Const contentDataSource As String = "default"   '<------ set to datasource if not default
+        '
+        '====================================================================================================
+        ' -- instance properties
+        Public Property catalogItemId As Integer
+        Public Property ContentCategoryID As Integer
+        Public Property durationtext As String
+        Public Property filesize As Integer
+        Public Property imageFilename As String
+        Public Property isBlack As Boolean
+        Public Property isFree As Boolean
+        Public Property isNetwork As Boolean
+        Public Property isPremium As Boolean
+        Public Property iTunesDownloadLink As String
+        Public Property lastLinkAlias As String
+        Public Property link As String
+        Public Property paypalButton As String
+        Public Property podcastDate As Date
+        Public Property PodcastMediaLink As String
+        Public Property RSSDateExpire As Date
+        Public Property RSSDatePublish As Date
+        Public Property RSSDescription As String
+        Public Property RSSLink As String
+        Public Property createdBy As String
+        '
+        '====================================================================================================
+        Public Overloads Shared Function add(cp As CPBaseClass) As BoadPodcastModel
+            Return add(Of BoadPodcastModel)(cp)
+        End Function
+        '
+        '====================================================================================================
+        Public Overloads Shared Function create(cp As CPBaseClass, recordId As Integer) As BoadPodcastModel
+            Return create(Of BoadPodcastModel)(cp, recordId)
+        End Function
+        '
+        '====================================================================================================
+        Public Overloads Shared Function create(cp As CPBaseClass, recordGuid As String) As BoadPodcastModel
+            Return create(Of BoadPodcastModel)(cp, recordGuid)
+        End Function
+        '
+        '====================================================================================================
+        Public Overloads Shared Function createByName(cp As CPBaseClass, recordName As String) As BoadPodcastModel
+            Return createByName(Of BoadPodcastModel)(cp, recordName)
+        End Function
+        '
+        '====================================================================================================
+        Public Overloads Sub save(cp As CPBaseClass)
+            MyBase.save(cp)
+        End Sub
+        '
+        '====================================================================================================
+        Public Overloads Shared Sub delete(cp As CPBaseClass, recordId As Integer)
+            delete(Of BoadPodcastModel)(cp, recordId)
+        End Sub
+        '
+        '====================================================================================================
+        Public Overloads Shared Sub delete(cp As CPBaseClass, ccGuid As String)
+            delete(Of BoadPodcastModel)(cp, ccGuid)
+        End Sub
+        '
+        '====================================================================================================
+        Public Overloads Shared Function createList(cp As CPBaseClass, sqlCriteria As String, Optional sqlOrderBy As String = "id") As List(Of BoadPodcastModel)
+            Return createList(Of BoadPodcastModel)(cp, sqlCriteria, sqlOrderBy)
+        End Function
+        '
+        '====================================================================================================
+        Public Overloads Shared Function getRecordName(cp As CPBaseClass, recordId As Integer) As String
+            Return baseModel.getRecordName(Of BoadPodcastModel)(cp, recordId)
+        End Function
+        '
+        '====================================================================================================
+        Public Overloads Shared Function getRecordName(cp As CPBaseClass, ccGuid As String) As String
+            Return baseModel.getRecordName(Of BoadPodcastModel)(cp, ccGuid)
+        End Function
+        '
+        '====================================================================================================
+        Public Overloads Shared Function getRecordId(cp As CPBaseClass, ccGuid As String) As Integer
+            Return baseModel.getRecordId(Of BoadPodcastModel)(cp, ccGuid)
+        End Function
+        '
+        '====================================================================================================
+        Public Overloads Shared Function getCount(cp As CPBaseClass, sqlCriteria As String) As Integer
+            Return baseModel.getCount(Of BoadPodcastModel)(cp, sqlCriteria)
+        End Function
+        '
+        '====================================================================================================
+        Public Overloads Function getUploadPath(fieldName As String) As String
+            Return MyBase.getUploadPath(Of BoadPodcastModel)(fieldName)
+        End Function
+        '
+        '====================================================================================================
+        '
+        Public Function Clone(cp As CPBaseClass) As BoadPodcastModel
+            Dim result As BoadPodcastModel = DirectCast(Me.Clone(), BoadPodcastModel)
+            result.id = cp.Content.AddRecord(contentName)
+            result.ccguid = cp.Utils.CreateGuid()
+            result.save(cp)
+            Return result
+        End Function
+        '
+        '====================================================================================================
+        '
+        Public Function Clone() As Object Implements ICloneable.Clone
+            Return Me.MemberwiseClone()
+        End Function
+
+    End Class
+End Namespace
